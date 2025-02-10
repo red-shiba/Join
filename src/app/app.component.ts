45f5aff71a146172bd07466a4c48786cb0abfb75
Michael Spari <michaelspari72@gmail.com>
2025-02-10 14:07:10 +0100

    Struktur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { DashBoardComponent } from './main-content/dash-board/dash-board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, DashBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
