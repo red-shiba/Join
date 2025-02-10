f0a155f198ed92daf05b97747798821f7fb1d322
Dzmitry Stashkevich <dimarehfeldt@icloud.com>
2025-02-10 17:30:45 +0100

    Contact-list

--- src/app/main-content/dash-board/dash-board.component.ts
+++ src/app/main-content/dash-board/dash-board.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-dash-board',
   standalone: true,
-  imports: [],
+  imports: [ContactsComponent],
   templateUrl: './dash-board.component.html',
-  styleUrl: './dash-board.component.scss'
+  styleUrl: './dash-board.component.scss',
 })
-export class DashBoardComponent {
-
-}
+export class DashBoardComponent {}
