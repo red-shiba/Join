d5a7221710f8125aff00c807fbaef4e93f637762
Dzmitry Stashkevich <dimarehfeldt@icloud.com>
2025-02-10 18:36:37 +0100

    contact-list& hover

--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
 export class ContactsComponent {
-
+  // selectedContact: Contact | null = null;
+  // selectContact(contact: Contact) {
+  //   this.selectedContact = contact;
+  // }
+  // // Alphabetische Sortierung der Gruppen
+  // Object.keys(this.contacts).forEach(letter => {
+  //   this.contacts[letter].sort((a, b) => a.name.localeCompare(b.name));
+  // });
 }
