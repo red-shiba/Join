58ea39d7c653bc00d32ad057dbafd7ee70b4f206
Michael Spari <michaelspari72@gmail.com>
2025-02-11 10:48:36 +0100

    Firebase install

---
+++ src/app/main-content/single-contact/single-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-contact.component.html',
+  styleUrl: './single-contact.component.scss'
+})
+export class SingleContactComponent {
+
+}
