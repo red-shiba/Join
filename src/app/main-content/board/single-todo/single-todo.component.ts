8a019f314b4a64eab7dcdd0085f5e187878a40c9
Michael Spari <michaelspari72@gmail.com>
2025-02-20 14:23:07 +0100

    single-todo.component

---
+++ src/app/main-content/board/single-todo/single-todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-todo.component.html',
+  styleUrl: './single-todo.component.scss'
+})
+export class SingleTodoComponent {
+
+}
