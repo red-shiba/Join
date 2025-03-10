1f6367d5e6b1bd0d7f68b3b656a7827cf07abf59
Dzmitry Stashkevich <dimarehfeldt@icloud.com>
2025-03-10 15:49:45 +0100

    summary-welcome-animation

--- src/app/main-content/summary/summary.component.ts
+++ src/app/main-content/summary/summary.component.ts
@@ -2,18 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../firebase-service/auth.service';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 @Component({
   selector: 'app-summary',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss',
+  animations: [
+    trigger('moveText', [
+      state(
+        'center',
+        style({
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'moved',
+        style({
+          top: '60px',
+          left: '77px',
+          transform: 'translate(0, 0)',
+          opacity: 1,
+        })
+      ),
+      transition('center => moved', animate('0.5s ease-in-out')),
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-in-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class SummaryComponent implements OnInit {
   displayName: string | null = '';
   greeting: string = '';
-
+  textState: 'center' | 'moved' = 'center';
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
@@ -22,6 +57,11 @@ export class SummaryComponent implements OnInit {
       this.displayName = user.displayName;
     }
     this.greeting = this.getGreeting();
+
+    // Automatisches Verschieben nach kurzer Verzögerung
+    setTimeout(() => {
+      this.textState = 'moved';
+    }, 1000);
   }
 
   getGreeting(): string {
