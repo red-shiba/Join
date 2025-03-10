5e81e7828e2ae0f614d363e5fb45154749289e4a
Michael Spari <michaelspari72@gmail.com>
2025-03-10 09:32:10 +0100

    form validation login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,14 @@ import { AuthService } from './../firebase-service/auth.service';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class LoginComponent {
 
@@ -18,8 +19,29 @@ export class LoginComponent {
   authService = inject(AuthService);
   email = '';
   password = '';
+  emailError = '';
+  passwordError = '';
+  generalError = '';
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+  emailPatternError = 'Please enter a valid email address.';
+  emailTouched = false;
+  passwordTouched = false;
 
   async login() {
+    this.emailError = '';
+    this.passwordError = '';
+    this.generalError = '';
+
+    if (!this.email) {
+      this.emailError = 'Email is required.';
+    }
+    if (!this.password) {
+      this.passwordError = 'Password is required.';
+    }
+    if (this.emailError || this.passwordError) {
+      return;
+    }
+
     try {
       await this.authService.login(this.email, this.password);
       
@@ -29,13 +51,30 @@ export class LoginComponent {
       alert('Login erfolgreich!');
       this.router.navigate(['/board']); // Weiterleitung zur Haupt-App
     } catch (error: any) {
-      alert(error.message);
+      this.generalError = error.message;
     }
   }
 
   guestLogin() {
     this.email = 'michaelspari@gmx.de';
     this.password = 'gast123';
+  }
+
+  validateEmail() {
+    this.emailError = '';
+    if (!this.email) {
+      this.emailError = 'Email is required.';
+    }
+    if (this.email && !this.email.match(this.emailPattern)) {
+      this.emailError = this.emailPatternError;
+    }
+  }
+
+  validatePassword() {
+    this.passwordError = '';
+    if (!this.password) {
+      this.passwordError = 'Password is required.';
+    }
   }
 }
 
