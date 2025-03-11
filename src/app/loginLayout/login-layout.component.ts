0e874e3ad3f6c27aa9a900c1b08121f4a1da39ec
Michael Spari <michaelspari72@gmail.com>
2025-03-11 12:40:41 +0100

    login navigation button

--- src/app/loginLayout/login-layout.component.ts
+++ src/app/loginLayout/login-layout.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { RouterLinkActive } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './login-layout.component.html',
   styleUrls: ['./login-layout.component.scss'],
   animations: [
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(private location: Location) {}
+  isLegalRoute = false;
+  isMainFrame = false;
+
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isLegalRoute = event.url.includes('legal');
+        this.isMainFrame = event.url.includes('/board') || event.url.includes('/summary') || event.url.includes('/contacts') || event.url.includes('/add-task');
+      }
+    });
+  }
 
   isActive(route: string): boolean {
     return this.location.path() === route;
