cedcc67170831db1e01391cd82567416d5889cbe
Cedeta <caglar.e89@gmail.com>
2025-03-12 16:26:51 +0100

    adding mobile Hover & Active style

--- src/app/main-frame/main-frame.component.ts
+++ src/app/main-frame/main-frame.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { HeaderComponent } from './../shared/header/header.component';
 import { NavbarComponent } from './../shared/navbar/navbar.component';
 
@@ -12,11 +12,16 @@ import { NavbarComponent } from './../shared/navbar/navbar.component';
     RouterOutlet,
     HeaderComponent,
     NavbarComponent,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './main-frame.component.html',
-  styleUrl: './main-frame.component.scss'
+  styleUrl: './main-frame.component.scss',
 })
 export class MainFrameComponent {
+  constructor(private router: Router) {}
 
+  // Prüft ob die Route aktiv ist
+  isActive(route: string): boolean {
+    return this.router.isActive(route, true);
+  }
 }
